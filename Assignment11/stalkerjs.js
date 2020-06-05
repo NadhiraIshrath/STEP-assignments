da5dfa8817c8927a305ac169eedc59453a56966d
NadhiraIshrath <53572893+NadhiraIshrath@users.noreply.github.com>
2020-06-05 22:11:50 +0530

    Add files via upload

---
+++ Assignment11/stalkerjs.js
@@ -0,0 +1,5 @@
+var firstname=prompt("What's your First Name?");
+var lastname=prompt("What's your Last Name?");
+var age=prompt("What's your age?");
+console.log("Name: "+firstname+" "+lastname);
+console.log("Age: "+age);
