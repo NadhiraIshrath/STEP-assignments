76e10a3cbe9fa2a90509334aa5a5e761c81ce273
NadhiraIshrath <53572893+NadhiraIshrath@users.noreply.github.com>
2020-06-08 22:13:13 +0530

    Add files via upload

---
+++ Assignment15/c.js
@@ -0,0 +1,15 @@
+ var button=document.querySelector("button");
+ // var iswhite=true;
+ button.addEventListener("click",function(){
+	//var bg=document.querySelector("body");
+	// if(iswhite){
+	//  document.body.style.background="#800080";
+	// }else{
+	//  //alert("CLICK");
+	//  document.body.style.background="white";
+	// }
+	// iswhite=!iswhite;
+	document.body.classList.toggle("purple");
+
+ });
+//alert("CONNECTED");
